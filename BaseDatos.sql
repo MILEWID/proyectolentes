b8de617f589e10fc99608d88b7f52ce3f032a873
SebasMena28 <78992252+SebasMena28@users.noreply.github.com>
2021-02-19 09:39:52 -0500

    Update BaseDatos.sql

---
+++ BaseDatos.sql
@@ -0,0 +1,104 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 19-02-2021 a las 02:50:26
+-- Versión del servidor: 10.4.11-MariaDB
+-- Versión de PHP: 7.4.5
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `bdoptica`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `producto`
+--
+
+CREATE TABLE `producto` (
+  `id` int(100) NOT NULL,
+  `nombre` varchar(250) NOT NULL,
+  `marca` varchar(50) NOT NULL,
+  `tipo` varchar(50) NOT NULL,
+  `precio` float NOT NULL,
+  `material` varchar(50) NOT NULL,
+  `sexo` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `nombreUsuario` varchar(50) NOT NULL,
+  `idUsuario` int(250) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `apellido` varchar(50) NOT NULL,
+  `tipoUsuario` varchar(50) NOT NULL,
+  `correoElectronico` varchar(100) NOT NULL,
+  `contrasena` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `usuario`
+--
+
+INSERT INTO `usuario` (`nombreUsuario`, `idUsuario`, `nombre`, `apellido`, `tipoUsuario`, `correoElectronico`, `contrasena`) VALUES
+('maria', 5, 'Maria', 'Cando', '1', 'ejemplo@correo.com', 'asdf'),
+('maria1', 7, 'Maria', 'Cando', '1', 'ejemplo1@correo.com', 'fwqe'),
+('sebas', 8, 'Sebas', 'Mena', '2', 'sebas@gmail.com', 'sebas'),
+('jessymilena', 9, 'Jessy', 'Ballesteros', 'Administrador', 'jessy@milena.com', 'asdbasd45565+'),
+('maria2', 10, 'Maria', 'Cando', 'Cliente', 'ejemplo2@correo.com', 'ad');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `producto`
+--
+ALTER TABLE `producto`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`idUsuario`),
+  ADD UNIQUE KEY `nombreUsuario` (`nombreUsuario`),
+  ADD UNIQUE KEY `correo` (`correoElectronico`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `producto`
+--
+ALTER TABLE `producto`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `idUsuario` int(250) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
