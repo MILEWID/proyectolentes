aa79b38b0df543a89044c74f4c880f6b1dab1baf
Dennys Rojas <dennys.rojas@espoch.edu.ec>
2021-02-25 13:14:50 -0500

    Update reportes.php
    correccion leyenda

--- Optica/Vista/html/reportes.php
+++ Optica/Vista/html/reportes.php
@@ -100,8 +100,8 @@
             <div class="col-sm-4">
                 <div class="card">
                     <div class="card-body">
-                        <h5 class="card-title">Reporte de clientes</h5>
-                        <p class="card-text">Reporte de la totalidad de clientes .</p>
+                        <h5 class="card-title">Reporte de usuarios</h5>
+                        <p class="card-text">Reporte de la totalidad de usuarios .</p>
                         <a href="<?php echo URL ?>reportes/reporteusuarios" class="btn btn-dark">Descargar pdf
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
                                 class="bi bi-file-earmark-text-fill" viewBox="0 0 16 16">
