391989c23356d4b08f7d9d19cdd2db98bd0bf1c3
Katty <kattygm10@hotmail.com>
2021-02-26 01:52:14 -0500

    ACERCA DE

--- Optica/Vista/html/Cliente-Registrarse.php
+++ Optica/Vista/html/Cliente-Registrarse.php
@@ -34,7 +34,9 @@
               </a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#">ACERCA DE</a>
+  
+              
+              <a class="nav-link" href="#" >ACERCA DE</a>
             </li>             
             <li class="nav-item dropdown">
               <div class="dropdown">
