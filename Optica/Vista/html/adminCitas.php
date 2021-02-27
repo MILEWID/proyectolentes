0b85e5ae2efaee3f12213b0019b97b0348690b7f
SebasMena28 <78992252+SebasMena28@users.noreply.github.com>
2021-02-26 23:13:41 -0500

    dddihi
    iij

--- Optica/Vista/html/adminCitas.php
+++ Optica/Vista/html/adminCitas.php
@@ -133,7 +133,7 @@
     <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog">
             <div class="modal-content">
-                <form action="<?php echo URL?>citas/buscarcita" method="POST">
+                <form action="<?php echo URL?>citas/buscarcita2" method="POST">
                     <div class="modal-header">
                         <h5 class="modal-title" id="exampleModalLabel">Buscar Cita</h5>
                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
