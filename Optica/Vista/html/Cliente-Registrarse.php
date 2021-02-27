1753b2788aff57324fc5dc352d26d2c1a2c5305c
LuisDaga <luis.daga@espoch.edu.ec>
2021-02-26 20:06:41 -0500

    Update Cliente-Registrarse.php

--- Optica/Vista/html/Cliente-Registrarse.php
+++ Optica/Vista/html/Cliente-Registrarse.php
@@ -89,7 +89,7 @@
     <div class="modal-dialog" role="document">
       <div class="modal-content">
         <div class="modal-header">   
-          <h3 class="modal-title" id="exampleModalLabel" style="text-align: center;"><i class="fas fa-users"></i> CREAR  - ÓPTICA VICTORIA</h3>                  
+          <h3 class="modal-title" id="exampleModalLabel" style="text-align: center;"><i class="fas fa-users"></i> CREAR CUENTA</h3>                  
           <button type="button" class="close" data-dismiss="modal" aria-label="Close">
             <span aria-hidden="true">&times;</span>
           </button>
